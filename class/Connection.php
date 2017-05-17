b14c3d3377a4e64150c1edfa6c0d77bfa96c9f43
Cliff Dean <cliff.dean@eannovate.com>
2017-05-17 10:00:33 +0700

    change config db, root url

--- class/Connection.php
+++ class/Connection.php
@@ -1,21 +1,22 @@
-<?php 
+<?php
 class Connection{
-// Include database connection settings
-	 private $username = "bonitaje_develop";
-	 private $password = "bonitajewel88";
-	 private $hostname = "localhost";
-	 private $database = "bonitaje_tms";
+	// Include database connection settings
+	private $username = "tamara_dev";
+	private $password = "dev123";
+	private $hostname = "localhost";
+	private $database = "tamara_dokdig";
     
 	public function up(){
 		$this->dbh_system = mysql_connect($this->hostname, $this->username, $this->password) or die("Unable to connect to MySQL");
-	//print "Connected to MySQL<br>";
+		//print "Connected to MySQL<br>";
 		$this->selected = mysql_select_db($this->database, $this->dbh_system) or die("Could not fetch database");
-	//print "Select ganiko_users Database<br>";
-}
+		//print "Select ganiko_users Database<br>";
+	}
 
 	public function down(){
 		$dbh_system = mysql_connect($this->hostname, $this->username, $this->password);
 		mysql_close($dbh_system);
 	}
+
 }
 ?>
