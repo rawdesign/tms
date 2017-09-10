96e5403e976c803e59ff26081b91763589e3ee92
Cliff Dean Smith <dean11.cliff@gmail.com>
2017-09-11 05:57:10 +0700

    adjust action api edit profile with new field

--- api/v1.0/api_user.php
+++ api/v1.0/api_user.php
@@ -77,9 +77,102 @@ if(isset($_GET['action'])){
 				$N_auth_token = mysql_real_escape_string($_REQUEST['auth_token']);
 
 				$N_name = mysql_real_escape_string($_REQUEST['name']);
+				$N_card_name = mysql_real_escape_string($_REQUEST['card_name']);
+				$N_province = mysql_real_escape_string($_REQUEST['province']);
+				$N_city = mysql_real_escape_string($_REQUEST['city']);
+				$N_height = mysql_real_escape_string($_REQUEST['height']);
+				$N_weight = mysql_real_escape_string($_REQUEST['weight']);
+				$N_size = mysql_real_escape_string($_REQUEST['size']);
+				$N_work = mysql_real_escape_string($_REQUEST['work']);
+				$N_tempat_lahir = mysql_real_escape_string($_REQUEST['tempat_lahir']);
+				$N_birthday = mysql_real_escape_string($_REQUEST['birthday']);
+				$N_gender = mysql_real_escape_string($_REQUEST['gender']);
+				$N_status_menikah = mysql_real_escape_string($_REQUEST['status_menikah']);
+				$N_warga_negara = mysql_real_escape_string($_REQUEST['warga_negara']);
+				$N_address = mysql_real_escape_string($_REQUEST['address']);
+				$N_mail_address = mysql_real_escape_string($_REQUEST['mail_address']);
+				$N_religion = mysql_real_escape_string($_REQUEST['religion']);
+				$N_phone1 = mysql_real_escape_string($_REQUEST['phone1']);
+				$N_phone2 = mysql_real_escape_string($_REQUEST['phone2']);
+				$N_phone3 = mysql_real_escape_string($_REQUEST['phone3']);
+				$N_npwp = mysql_real_escape_string($_REQUEST['npwp']);
+				$N_instagram = mysql_real_escape_string($_REQUEST['instagram']);
+				$N_whatsapp = mysql_real_escape_string($_REQUEST['whatsapp']);
+				$N_bank = mysql_real_escape_string($_REQUEST['bank']);
+				$N_cabang = mysql_real_escape_string($_REQUEST['cabang']);
+				$N_no_rek = mysql_real_escape_string($_REQUEST['no_rek']);
+				$image_loc1 = "";
+				$image_locThmb1 = "";
+				$scan_loc1 = "";
+				$scan_locThmb1 = "";
+
+				require_once($global['root-url']."packages/SimpleImage.php"); // class simple image
+				if(isset($_FILES['image']['name'])){
+					if(!empty($_FILES['image']['name'])){
+						$allowed_ext = array('jpg', 'jpeg', 'png', 'gif');
+						$file_name = cleanSpace($_FILES['image']['name']);
+						$file_ext = strtolower(end(explode('.', $file_name)));
+						$file_size = $_FILES['image']['size'];
+						$file_tmp = $_FILES['image']['tmp_name'];
+						$ran = rand();
+			    		$timestamp = time();
+						
+						if(in_array($file_ext, $allowed_ext) === true){
+							if($file_size < 10044070){
+								//save image in server
+								$image_loc = $global['root-url']."uploads/user/".$timestamp.$ran.$file_name; 
+								$image_locThmb = $global['root-url']."uploads/user-thmb/".$timestamp.$ran.$file_name;
+
+								//save image in database
+								$image_loc1 = "uploads/user/".$timestamp.$ran.$file_name; 
+								$image_locThmb1 = "uploads/user-thmb/".$timestamp.$ran.$file_name;
+
+								if(move_uploaded_file($file_tmp, $image_loc))
+								{
+									$image = new SimpleImage();
+									$image->load($image_loc);
+									$image->resize(200,200);
+									$image->save($image_locThmb);
+								}
+							}
+						}
+					}
+				}
+
+				if(isset($_FILES['scan']['name'])){
+					if(!empty($_FILES['scan']['name'])){
+						$allowed_ext = array('jpg', 'jpeg', 'png', 'gif');
+						$file_name = cleanSpace($_FILES['scan']['name']);
+						$file_ext = strtolower(end(explode('.', $file_name)));
+						$file_size = $_FILES['scan']['size'];
+						$file_tmp = $_FILES['scan']['tmp_name'];
+						$ran = rand();
+			    		$timestamp = time();
+						
+						if(in_array($file_ext, $allowed_ext) === true){
+							if($file_size < 10044070){
+								//save image in server
+								$scan_loc = $global['root-url']."uploads/user/".$timestamp.$ran.$file_name; 
+								$scan_locThmb = $global['root-url']."uploads/user-thmb/".$timestamp.$ran.$file_name;
+
+								//save image in database
+								$scan_loc1 = "uploads/user/".$timestamp.$ran.$file_name; 
+								$scan_locThmb1 = "uploads/user-thmb/".$timestamp.$ran.$file_name;
+
+								if(move_uploaded_file($file_tmp, $scan_loc))
+								{
+									$image = new SimpleImage();
+									$image->load($scan_loc);
+									$image->resize(200,200);
+									$image->save($scan_locThmb);
+								}
+							}
+						}
+					}
+				}
 
 				if($obj_user->check_code($N_auth_token, $N_user_id)){//check code
-					$result = $obj_user->update_data($N_user_id, $N_name);
+					$result = $obj_user->update_data($N_user_id, $N_name, $N_card_name, $N_province, $N_city, $N_height, $N_weight, $N_size, $N_work, $N_tempat_lahir, $N_birthday, $N_gender, $N_status_menikah, $N_warga_negara, $N_address, $N_mail_address, $N_religion, $N_phone1, $N_phone2, $N_phone3, $N_npwp, $N_instagram, $N_whatsapp, $N_bank, $N_cabang, $N_no_rek, $image_loc1, $image_locThmb1, $scan_loc1, $scan_locThmb1, $global['root-url']);
 					if($result == 1){
 						$R_message = array("status" => "200", "message" => "Edit profile success");
 					}
--- class/v1.0/User.php
+++ class/v1.0/User.php
@@ -58,10 +58,22 @@ class User{
         return $result;
     }
 
-    public function update_data($id, $name){
+    public function update_data($id, $name, $card_name, $province, $city, $height, $weight, $size, $work, $tempat_lahir, $birthday, $gender, $status_menikah, $warga_negara, $address, $mail_address, $religion, $phone1, $phone2, $phone3, $npwp, $instagram, $whatsapp, $bank, $cabang, $no_rek, $img, $img_thmb, $scan, $scan_thmb, $path){
         $result = 0;
+        $condImg = "";
+        if($img != ""){
+            $this->remove_image_field($id, "user_img", "user_img_thmb", $path);
+            $condImg = ", user_img = '$img', user_img_thmb = '$img_thmb'";
+        }
+        $condScan = "";
+        if($scan != ""){
+            $this->remove_image_field($id, "user_scan", "user_scan_thmb", $path);
+            $condScan = ", user_scan = '$scan', user_scan_thmb = '$scan_thmb'";
+        }
 
-        $text = "UPDATE $this->table SET user_name = '$name' WHERE user_id = '$id'";
+        $text = "UPDATE $this->table SET user_name = '$name', user_card_name = '$card_name', user_province = '$province', user_city = '$city', user_height = '$height', user_weight = '$weight', user_size = '$size', user_work = '$work', user_tempat_lahir = '$tempat_lahir', user_birthday = '$birthday', user_gender = '$gender',
+            user_status_menikah = '$status_menikah', user_warga_negara = '$warga_negara', user_address = '$address', user_mail_address = '$mail_address', user_religion = '$religion', user_phone1 = '$phone1', user_phone2 = '$phone2', user_phone3 = '$phone3', user_npwp = '$npwp', user_instagram = '$instagram', user_whatsapp = '$whatsapp',
+            user_bank = '$bank', user_cabang = '$cabang', user_no_rek = '$no_rek' $condImg $condScan WHERE user_id = '$id'";
         $query = mysql_query($text);    
         if(mysql_affected_rows() == 1){
             $result = 1;
@@ -76,6 +88,36 @@ class User{
         $query = mysql_query($text);
         if(mysql_affected_rows() == 1){
             $result = 1;
+        }
+        return $result;
+    }
+
+    public function remove_image_field($id, $field, $field_thmb, $path){
+        $result = 0;
+        $flag_img = 0;
+        $flag_img_thmb = 0;
+
+        $text = "SELECT $field, $field_thmb FROM $this->table WHERE user_id = '$id'";
+        $query = mysql_query($text);
+        if(mysql_num_rows($query) == 1){
+            $row = mysql_fetch_assoc($query);
+            if($row[$field] != "" && $row[$field_thmb] != ""){
+                $deleteImg = $path.$row[$field];
+                if (file_exists($deleteImg)) {
+                    unlink($deleteImg);
+                    $flag_img = 1;
+                }
+
+                $deleteImgThmb = $path.$row[$field_thmb];
+                if (file_exists($deleteImgThmb)) {
+                    unlink($deleteImgThmb);
+                    $flag_img_thmb = 1;
+                }
+                
+                if($flag_img == 1 && $flag_img_thmb ==1){
+                    $result = 1;
+                }
+            }
         }
         return $result;
     }
